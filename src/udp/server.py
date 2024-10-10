b9ceb06e186a761fac01411549913ef60d852ffe
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2024-10-10 13:02:53 -0600

    add: udp server
    Just the initial source, only starts the server but it doesnt do
    anything yet

---
+++ src/udp/server.py
@@ -0,0 +1,30 @@
+import threading
+import datetime
+import socket
+import sys
+import os 
+import colorama
+
+from colorama import init, Fore, Style
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from utils.index import generate_random_port
+from utils.index import get_machine_ip
+
+server_config = (
+  get_machine_ip(),
+  generate_random_port()
+)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+server.bind(server_config)
+server.listen(1)
+
+init()
+
+print(f"{Fore.GREEN}Servidor iniciado en {server_config[0]}:{server_config[1]}, esperando conexiones..")
+
+clients = []
+stop_server = False

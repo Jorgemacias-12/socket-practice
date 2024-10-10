4a535a0a90bfbbe6c077ae783cb7814d09945a0a
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2024-10-10 15:59:55 -0600

    fix: ip address get not working
    Use psutils to ignore vEthernet interfaces

--- src/utils/index.py
+++ src/utils/index.py
@@ -1,5 +1,6 @@
 import random
 import socket
+import psutil
 
 def generate_random_port():
   return random.randint(1024, 65535)
@@ -28,7 +29,13 @@ def get_machine_ip():
   
   ips = socket.gethostbyname_ex(local_hostname)[2]
   
-  filtered_ips = [ip for ip in ips if not ip.startswith("127.")]
+  filtered_ips = []
+  for ip in ips:
+    if not ip.startswith("127."):
+      for iface, addrs in psutil.net_if_addrs().items():
+        if "vEthernet" not in iface:
+          for addr in addrs:
+            if addr.address == ip:
+              filtered_ips.append(ip)
   
-  return filtered_ips[:1][0]
-  
+  return filtered_ips[:1][0] if filtered_ips else None

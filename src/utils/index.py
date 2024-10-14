77ef9359910296ba74c23614c2263f35ad497a2e
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2024-10-14 11:36:53 -0600

    update: improve list_folder_contents
    and implement convert_to_table
    
    The first function now retuns a list of a record/tuple that contains the
    name of the file and his size in KB.
    
    The second one uses tabulate module to make a table and then use it in
    the server to send it to the client and then show it.

--- src/utils/index.py
+++ src/utils/index.py
@@ -1,6 +1,9 @@
 import random
 import socket
 import psutil
+import os
+
+from tabulate import tabulate
 
 def generate_random_port():
   return random.randint(1024, 65535)
@@ -39,3 +42,36 @@ def get_machine_ip():
               filtered_ips.append(ip)
   
   return filtered_ips[:1][0] if filtered_ips else None
+
+def list_folder_contents():
+  user_docs = os.path.join(os.environ['USERPROFILE'], 'Documents')
+  
+  folder_name = "udp_documents"
+  
+  folder_path = os.path.join(user_docs, folder_name)
+  
+  if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+  
+  files = os.listdir(folder_path)
+  
+  file_details = []
+  
+  for file in files:
+    file_path = os.path.join(folder_path, file)
+    
+    if not os.path.isfile(file_path):
+      continue
+    
+    size = os.path.getsize(file_path)
+    file_details.append([file, f"{size / 1024:.2f} KB"])
+  
+  return file_details
+
+def convert_to_table(data):
+  
+  headers = ["Filename", "Size"]
+  
+  table = tabulate(data, headers, tablefmt="grid")
+  
+  return table
